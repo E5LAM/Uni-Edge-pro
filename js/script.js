645b06409a610399df7abbf8ad9b4677bad9d76b
Eslam-Hussien <eslam01003733553@gmail.com>
2023-12-03 17:51:41 +0200

    add files

---
+++ js/script.js
@@ -0,0 +1,97 @@
+$(document).ready(function() {
+    $('.sale-box').children("span").eq(0).mouseover(function() {
+      $('.function-section').eq(0).children(".function-box").eq(0).css({
+        'box-shadow': '5px 5px 10px rgba(41, 41, 41, 0.541) , -5px -5px 10px rgba(41, 41, 41, 0.541) '
+      });
+    });
+  
+    $('.sale-box').children("span").eq(0).mouseout(function() {
+        $('.function-section').eq(0).children(".function-box").eq(0).css({
+            'box-shadow': ''
+      });
+    });
+  });
+  ///////////////////////////////////
+$(document).ready(function() {
+    $('.sale-box').children("span").eq(1).mouseover(function() {
+      $('.function-section').eq(0).children(".function-box").eq(1).css({
+        'box-shadow': '5px 5px 10px rgba(41, 41, 41, 0.541) , -5px -5px 10px rgba(41, 41, 41, 0.541) '
+      });
+    });
+  
+    $('.sale-box').children("span").eq(1).mouseout(function() {
+        $('.function-section').eq(0).children(".function-box").eq(1).css({
+            'box-shadow': ''
+      });
+    });
+  });
+///////////////////////////////
+$(document).ready(function() {
+    $('.sale-box').children("span").eq(2).mouseover(function() {
+      $('.function-section').eq(0).children(".function-box").eq(2).css({
+        'box-shadow': '5px 5px 10px rgba(41, 41, 41, 0.541) , -5px -5px 10px rgba(41, 41, 41, 0.541) '
+      });
+    });
+  
+    $('.sale-box').children("span").eq(2).mouseout(function() {
+        $('.function-section').eq(0).children(".function-box").eq(2).css({
+            'box-shadow': ''
+      });
+    });
+  });
+///////////////////////////////
+$(document).ready(function() {
+    $('.sale-box').children("span").eq(3).mouseover(function() {
+      $('.function-section').eq(0).children(".function-box").eq(3).css({
+        'box-shadow': '5px 5px 10px rgba(41, 41, 41, 0.541) , -5px -5px 10px rgba(41, 41, 41, 0.541) '
+      });
+    });
+  
+    $('.sale-box').children("span").eq(3).mouseout(function() {
+        $('.function-section').eq(0).children(".function-box").eq(3).css({
+            'box-shadow': ''
+      });
+    });
+  });
+
+  $(document).ready(function() {
+    $('.sale-box').children("span").eq(4).mouseover(function() {
+      $('.function-section').eq(1).children(".function-box").eq(2).css({
+        'box-shadow': '5px 5px 10px rgba(41, 41, 41, 0.541) , -5px -5px 10px rgba(41, 41, 41, 0.541) '
+      });
+    });
+  
+    $('.sale-box').children("span").eq(4).mouseout(function() {
+        $('.function-section').eq(1).children(".function-box").eq(2).css({
+            'box-shadow': ''
+      });
+    });
+  });
+
+$(document).ready(function() {
+    $('.sale-box').children("span").eq(5).mouseover(function() {
+      $('.function-section').eq(1).children(".function-box").eq(1).css({
+        'box-shadow': '5px 5px 10px rgba(41, 41, 41, 0.541) , -5px -5px 10px rgba(41, 41, 41, 0.541) '
+      });
+    });
+  
+    $('.sale-box').children("span").eq(5).mouseout(function() {
+        $('.function-section').eq(1).children(".function-box").eq(1).css({
+            'box-shadow': ''
+      });
+    });
+  });
+
+$(document).ready(function() {
+    $('.sale-box').children("span").eq(6).mouseover(function() {
+      $('.function-section').eq(1).children(".function-box").eq(0).css({
+        'box-shadow': '5px 5px 10px rgba(41, 41, 41, 0.541) , -5px -5px 10px rgba(41, 41, 41, 0.541) '
+      });
+    });
+  
+    $('.sale-box').children("span").eq(6).mouseout(function() {
+        $('.function-section').eq(1).children(".function-box").eq(0).css({
+            'box-shadow': ''
+      });
+    });
+  });
