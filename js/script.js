dcc2e4f2156cfe6717c77c64f89b59482852b3df
Eslam-Hussien <eslam01003733553@gmail.com>
2023-12-14 10:48:12 +0200

    add image slider in service page

--- js/script.js
+++ js/script.js
@@ -119,20 +119,8 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
-// $(document).ready(function() {
-//   $(".accordion").click(function() {
-//     if ($(".panel").st("min-width")) {
-//       $(this).children("span").removeClass("min-width");
-//       $(this).children("span").animate({ width:"68%" }, 1000);
-//       $("#drop-down").slideDown(1000);
-//     } else {
-//       $(this).children("span").addClass("min-width");
-//       $(this).children("span").animate({ width: "40%" }, 1000);
-//       $("#drop-down").slideUp(1000);
-//     }
-//   });
-// });
 
 function transform(x){
   x.classList.toggle("change")
 }
+
